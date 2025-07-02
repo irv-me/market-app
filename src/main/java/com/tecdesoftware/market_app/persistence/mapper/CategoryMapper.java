31ab3a8ff8c25fe19ee9a85fa0a7f278d6315e0d
irv-me <tareastareas89@gmail.com>
2025-07-02 13:31:19 -0600

    Mapstruct Implemented

---
+++ src/main/java/com/tecdesoftware/market_app/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,4 @@
+package com.tecdesoftware.market_app.persistence.mapper;
+
+public interface CategoryMapper {
+}
---
+++ src/main/java/com/tecdesoftware/market_app/persistence/mapper/ProductMapper.java
@@ -0,0 +1,5 @@
+package com.tecdesoftware.market_app.persistence.mapper;
+
+public interface ProductMapper {
+}
+//implemented
