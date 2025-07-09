d6635a4e6e5a88fc268472b2840d3ff42428bb44
irv-me <tareastareas89@gmail.com>
2025-07-09 12:29:29 -0600

    Creation of PurchaseRepository

---
+++ src/main/java/com/tecdesoftware/market_app/domain/PurchaseItem.java
@@ -0,0 +1,40 @@
+package com.tecdesoftware.market_app.domain;
+
+public class PurchaseItem {
+    private int product_id;
+    private int quantity;
+    private double total;
+    private boolean active;
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
